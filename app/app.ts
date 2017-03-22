36765a8fb02f2b25fda4a76d4ade4f8393aec589
Sergio Cruz <hello@sergiocruz.me>
2017-03-22 18:46:18 -0500

    Installed & bootstraped Angular

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,16 @@
 const angular = require('angular');
 import * as uiRouter from 'angular-ui-router';
 import { weatherModule } from './weather';
+import { AppModule } from './ng-upgrade'
+import { UpgradeModule } from '@angular/upgrade/static';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 angular.module('app', [
   uiRouter,
   weatherModule
 ]);
+
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.body, ['app']);
+});
