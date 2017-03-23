d2e8ee3f3368122ee883b2c9149cfc822c782d89
Sergio Cruz <hello@sergiocruz.me>
2017-03-23 02:08:13 -0500

    Removed unneeded filter

--- app/weather/weather-preview.js
+++ app/weather/weather-preview.js
@@ -9,7 +9,7 @@ function WeatherPreviewDirective(Weather) {
     template: `
       <div class="weather-preview" ng-if="weather">
         <i class="wi {{ Weather.getIcon(weather.item.condition) }}"></i>
-        It's {{ weather.item.condition.temp | number:0 }}°{{ weather.units.temperature }} in
+        It's {{ weather.item.condition.temp }}°{{ weather.units.temperature }} in
         <a ui-sref="weather({ countryCode: weather.location.country, searchTerm: weather.location.city })">{{ weather.location.city }}</a>
         right now.
       </div>
