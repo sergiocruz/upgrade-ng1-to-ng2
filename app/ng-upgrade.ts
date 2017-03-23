215b1062d98a74ac22ce66065b11a5a34126e5dc
Sergio Cruz <hello@sergiocruz.me>
2017-03-23 01:30:18 -0500

    Leaving HttpModule in already

--- app/ng-upgrade.ts
+++ app/ng-upgrade.ts
@@ -3,12 +3,15 @@ import 'reflect-metadata';
 import { NgModule } from '@angular/core';
 import { UpgradeModule } from '@angular/upgrade/static';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     BrowserModule,
     UpgradeModule
   ]
+    UpgradeModule,
+    HttpModule
 })
 export class AppModule {
   ngDoBootstrap() {}
